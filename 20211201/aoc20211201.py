7df943de55789c46b7783cfde428c401aaacc121
Frederik Wilmotte <frederik.wilmotte@dxcfds.com>
2021-12-01 15:59:02 +0100

    Refactor solutions Day 1 (added sliding parameter)

--- 20211201/aoc20211201.py
+++ 20211201/aoc20211201.py
@@ -19,37 +19,21 @@ def create_array(filename):
     return array
 
 
-# --------------
-# --- Part 1 ---
-# --------------
-def sonar_sweep_pt1(filename):
+# -------------------
+# --- Sonar Sweep ---
+# -------------------
+def sonar_sweep(filename, sliding):
 
     sea_depth_array = create_array(filename)
 
     depth_increase = 0
-    while len(sea_depth_array) > 1:
-        if sea_depth_array[1] > sea_depth_array[0]:
+    while len(sea_depth_array) > sliding:
+        if sum(sea_depth_array[1:sliding+1]) > sum(sea_depth_array[0:sliding]):
             depth_increase += 1
         sea_depth_array.pop(0)
 
     return depth_increase
 
 
-# --------------
-# --- Part 2 ---
-# --------------
-def sonar_sweep_pt2(filename):
-
-    sea_depth_array = create_array(filename)
-
-    depth_increase = 0
-    while len(sea_depth_array) > 3:
-        if sum(sea_depth_array[1:4]) > sum(sea_depth_array[0:3]):
-            depth_increase += 1
-        sea_depth_array.pop(0)
-
-    return depth_increase
-
-
-print(sonar_sweep_pt1("sonarSweepReport_input"))
-print(sonar_sweep_pt2("sonarSweepReport_input"))
+print("Part 1: ", sonar_sweep("sonarSweepReport_input", 1))
+print("Part 2: ", sonar_sweep("sonarSweepReport_input", 3))
