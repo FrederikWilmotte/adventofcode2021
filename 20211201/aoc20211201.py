356110e60c477794d6be8c11a8ed6f692178dc28
Frederik Wilmotte <frederik.wilmotte@dxcfds.com>
2021-12-01 11:50:03 +0100

    Refactor solutions Day 1

--- 20211201/aoc20211201.py
+++ 20211201/aoc20211201.py
@@ -2,31 +2,38 @@
 # Author: Frederik Wilmotte
 # --- Day 1: Sonar Sweep ---
 
-sonarSweepReport = open("sonarSweepReport_input", "r")
+
 seaDepthArray = []
-address = 1
-address2 = 2
 depthIncrease = 0
 depthIncrease2 = 0
 
-# Load sea floor depth in array
-for seaDepth in sonarSweepReport:
-    seaDepthArray.append(int(seaDepth))
+
+def read_file():
+    return open("sonarSweepReport_input", "r")
 
 # Part 1
-while address < len(seaDepthArray):
-    if seaDepthArray[address] > seaDepthArray[address-1]:
+
+# Load sea floor depth in array
+seaDepthArray.clear()
+for seaDepth in read_file():
+    seaDepthArray.append(int(seaDepth))
+
+while len(seaDepthArray) > 1:
+    if seaDepthArray[1] > seaDepthArray[0]:
         depthIncrease += 1
-    address += 1
+    seaDepthArray.pop(0)
 
 # Part 2
-seaDepthSum = seaDepthArray[address2] + seaDepthArray[address2-1] + seaDepthArray[address2-2]
-address2 += 1
-while address2 < len(seaDepthArray):
-    if (seaDepthArray[address2] + seaDepthArray[address2-1] + seaDepthArray[address2-2]) > seaDepthSum:
+
+# Load sea floor depth in array
+seaDepthArray.clear()
+for seaDepth in read_file():
+    seaDepthArray.append(int(seaDepth))
+
+while len(seaDepthArray) > 3:
+    if sum(seaDepthArray[1:4]) > sum(seaDepthArray[0:3]):
         depthIncrease2 += 1
-    seaDepthSum = seaDepthArray[address2] + seaDepthArray[address2 - 1] + seaDepthArray[address2 - 2]
-    address2 += 1
+    seaDepthArray.pop(0)
 
 print("Number of depth increase - Part 1:", depthIncrease)
 print("Number of depth increase - Part 2:", depthIncrease2)
